c4ff65c7d275cf88dda3c6e694e5b2f53f40ab3b
I74L0 <i7alog4m3r1@gmail.com>
2024-09-28 18:33:58 -0300

    Added coins with animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -21,7 +21,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction
@@ -32,5 +32,8 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animation.play("idle")
+
+	if !(Input.is_action_pressed("left") or Input.is_action_pressed("right")):
+		velocity.x = 0
 
 	move_and_slide()
