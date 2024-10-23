2a77d9ad21de735a2da7d2da0e150c8947df7e38
i74l0 <i7alog4m3r1@gmail.com>
2024-10-23 11:01:09 -0300

    Added spikes

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -2,17 +2,10 @@ extends Camera2D
 
 @onready var node_to_follow: Node2D = get_node("../player")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#print("camera_position: ", self.position)
-	#print(node_to_follow.name, " position is: ",node_to_follow.position)
-	#print(node_to_follow.name, " remote position is: ",node_to_follow.get_node("remote").position)
-	position = node_to_follow.position
-	pass
+	if get_node_or_null("../player"):
+		position = node_to_follow.position
 
 func set_node_to_follow(node: NodePath) -> void:
 	node_to_follow = get_node(node)
