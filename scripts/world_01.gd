b227f7cf8e2eb1c21b698fabdaf07560554e76df
I74L0 <i7alog4m3r1@gmail.com>
2024-11-01 08:41:11 -0300

    Adding checkpoint

--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 @onready var player: CharacterBody2D = $player
+@onready var player_scene = preload("res://actors/player.tscn")
 @onready var control: Control = $HUD/control
+@onready var camera: Camera2D = $camera
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player.player_has_died.connect(reload_game)
+	camera.set_node_to_follow("../player")
+	Globals.player = player
+	Globals.player.player_has_died.connect(reload_game)
 	control.time_is_up.connect(reload_game)
 	Globals.coins = 0
 	Globals.score = 0
@@ -17,4 +21,9 @@ func _process(delta: float) -> void:
 
 func reload_game():
 	await get_tree().create_timer(1).timeout
-	get_tree().reload_current_scene()
+	var player = player_scene.instantiate()
+	add_child(player)
+	Globals.player = player
+	Globals.player.player_has_died.connect(reload_game)
+	Globals.respawn_player()
+	#get_tree().reload_current_scene()
--- singletons/globals.gd
+++ singletons/globals.gd
@@ -3,3 +3,11 @@ extends Node
 var coins: int = 0
 var score: int = 0
 var player_life: int = 5
+
+var player = null
+
+var current_checkpoint = null
+
+func respawn_player():
+	if current_checkpoint != null:
+		player.position = current_checkpoint.global_position
