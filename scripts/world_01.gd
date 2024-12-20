a621fa8fa4185a6f22ecb04229c6167a9cc19920
i74l0 <i7alog4m3r1@gmail.com>
2024-12-20 09:20:06 -0300

    Fixed minor bugs

--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -4,26 +4,30 @@ extends Node2D
 @onready var player_scene = preload("res://actors/player.tscn")
 @onready var control: Control = $HUD/control
 @onready var camera: Camera2D = $camera
+@onready var player_start_position: Marker2D = $player_start_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Globals.player_start_position = player_start_position
 	camera.set_node_to_follow("../player")
 	Globals.player = player
 	Globals.player.player_has_died.connect(reload_game)
-	control.time_is_up.connect(reload_game)
+	control.time_is_up.connect(game_over)
 
 func reload_game():
 	await get_tree().create_timer(1).timeout
 	var player = player_scene.instantiate()
 	add_child(player)
+	control.reset_clock_timer()
 	Globals.player = player
 	Globals.player.player_has_died.connect(reload_game)
 	Globals.coins = 0
 	Globals.score = 0
 	Globals.player_life = 5
 	Globals.respawn_player()
-	#get_tree().reload_current_scene()
 
+func game_over():
+	get_tree().reload_current_scene()
 
 func _on_cutscene_animation_finished(anim_name: StringName) -> void:
 	Globals.can_move = true
--- singletons/globals.gd
+++ singletons/globals.gd
@@ -7,8 +7,11 @@ var can_move: bool = true
 
 var player = null
 
+var player_start_position = null
 var current_checkpoint = null
 
 func respawn_player():
 	if current_checkpoint != null:
 		player.position = current_checkpoint.global_position
+	else:
+		player.global_position = player_start_position.global_position
